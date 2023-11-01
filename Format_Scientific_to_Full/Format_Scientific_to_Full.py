7e017118b8e681a16e16c1ebc3440ab9cd870282
Karthikeyan Ilango <31666260+karthik2411@users.noreply.github.com>
2023-11-01 15:17:28 +0000

    Format_Scientific_to_Full.py

--- Format_Scientific_to_Full/Format_Scientific_to_Full.py
+++ Format_Scientific_to_Full/Format_Scientific_to_Full.py
@@ -1,14 +1,14 @@
-# Define a function to format a number in scientific notation to a full number
 def format_scientific_to_full(value):
-    parts = value.split('.')
+    # Convert the value to a string
+    value_str = str(value)
+    parts = value_str.split('.')
     if len(parts) == 2:
         # If the value has a decimal point, remove trailing zeros
         integer_part, decimal_part = parts
         return integer_part.rstrip('0')
     else:
         # If there's no decimal point, return the original value
-        return value
-
+        return value_str
 
 
 # Apply the function to the 'Consignment' column
